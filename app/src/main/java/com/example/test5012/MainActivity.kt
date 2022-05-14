3d673263e35a484cce90460389a5cd15cb05f0f7
Klein <KjartanKlein@github.com>
2022-05-14 02:41:09 +0100

    Added many features

--- app/src/main/java/com/example/test5012/MainActivity.kt
+++ app/src/main/java/com/example/test5012/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.test5012
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
+import android.widget.LinearLayout.VERTICAL
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.google.firebase.firestore.FirebaseFirestore
-import org.w3c.dom.Text
 
+
+data class Project(
+    val worker: List<String> ,
+    val task: List<String>,
+    val state: List<String>,
+    val deadline:String = "",
+    val projectName:String = ""
+)
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var mDBOpenHelperProject: DBOpenHelperProject? = null
@@ -24,54 +33,124 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     class FirebaseUtils {
         val fireStoreDatabase = FirebaseFirestore.getInstance()
     }
+    private fun testWorker(map: Map<*,*>,user: String): Boolean {
+        val tester = map.get("worker").toString()
+        var booltest = false
+        if (tester.contains(",")) {
+            val worker_list = map.get("worker") as ArrayList<*>
+            for (i in 0 until worker_list.size) {
+                if (worker_list.get(i).toString().contains(user)) {
+                    booltest = true
+                    break
+                }
+            }
+        } else {
+            booltest = tester.contains(user)
+        }
+        return booltest
+    }
+    private fun createCard(workertester: String, projectlistall: Map<*,*>, user: String,list:LinearLayout,bt_id: Int): CardView {
+        if (workertester.contains(",")){
+            val workerlist = projectlistall.get("worker") as ArrayList<*>
+            val statelist = projectlistall.get("state") as ArrayList<*>
+            val tasklist = projectlistall.get("task") as ArrayList<*>
+            for (j in 0..workerlist.size-1) {
+                if (user.contains(workerlist[j].toString())&&tasklist[j].toString().contains("onGoing")) {
+                    val hlistV = LinearLayout(this)
+                    val TaskV = TextView(this)
+                    val StatusV = TextView(this)
+                    val WorkerV = TextView(this)
+                    TaskV.text = tasklist[j].toString()
+                    StatusV.text = statelist[j].toString()
+                    WorkerV.text = user
+                    val Button = Button(this)
+                    Button.id = bt_id;
+                    println("button id is : ${Button.id}")
+                    Button.setOnClickListener(this)
+                    hlistV.addView(TaskV)
+                    hlistV.addView(StatusV)
+                    hlistV.addView(WorkerV)
+                    hlistV.addView(Button)
+                    list.addView(hlistV)
+                }
 
-       private fun initView(user: String, pos: String){
+            }
+        }else{
+            val hlistV = LinearLayout(this)
+            val TaskV = TextView(this)
+            val StatusV = TextView(this)
+            val WorkerV = TextView(this)
+            TaskV.text = projectlistall["task"].toString()
+            StatusV.text =projectlistall["state"].toString()
+            WorkerV.text = user
+            val Button = Button(this)
+            Button.id = bt_id;
+            println("button id is : ${Button.id}")
+            Button.setOnClickListener(this)
+            TaskV.id = 100+bt_id
+            hlistV.addView(TaskV)
+            hlistV.addView(StatusV)
+            hlistV.addView(WorkerV)
+            hlistV.addView(Button)
+            list.addView(hlistV)
+        }
+        val cvCard = CardView(this)
+        cvCard.radius = 15f
+        cvCard.setCardBackgroundColor(Color.parseColor("#009688"))
+        cvCard.setContentPadding(36,36,36,36)
+        cvCard.cardElevation = 30f
+
+        cvCard.addView(list)
+        return cvCard
+    }
+    private fun initView(user: String, pos: String){
             val mBtMainLogout = findViewById<Button>(R.id.bt_main_logout)
             val newProj = findViewById<Button>(R.id.bt_main_create_new_project)
-
-            //val workerList = findViewById<TextView>(R.id.workerList)
-            //workerList.text = user
             newProj.isEnabled = false
             if (pos == "manager"){
                 newProj.isEnabled = true
             }       // get the layout and event
-           //var mlist = mDBOpenHelperProject?.getinfoByName(user)
 
-           //var currentProj = mDBOpenHelperProject?.getProjByName(user)
-           //var currentTask = mDBOpenHelperProject?.getTaskByName(user)
-           //var currentDDL = mDBOpenHelperProject?.getDDLByName(user)
-           //var currentState = mDBOpenHelperProject?.getStateByName(user)
-
-           var b = Bundle()
-           /////////////////////////////////////////////////////////////////////////////////////
-           FirebaseUtils().fireStoreDatabase.collection("projects")
-               .get()
-               .addOnSuccessListener { querySnapshot ->
+        var linear = findViewById<LinearLayout>(R.id.fragment_bucket)
+        var bt_id = 0
+           //////////////  ///////////////////////////////////////////////////////////////////////
+           var fb = FirebaseUtils().fireStoreDatabase.collection("projects")
+           fb.get().addOnSuccessListener { querySnapshot ->
                    querySnapshot.forEach { document ->
                        Log.d(TAG, "Read document with ID ${document.id}")
+                       var projectlistall = document.getData() as Map<*,*>
+                       val workertester = projectlistall["worker"].toString() //Checks if there is an array or string, crashes without
+                       val tasklist = projectlistall["task"]
+                       val statelist = projectlistall["state"]
+                       val workerlist = projectlistall["worker"]
+                       val name = projectlistall["projectName"].toString()
+                       val projdeadline = projectlistall["deadline"].toString()
 
-                       if (document.data["worker"] == user) {
+                       if (testWorker(projectlistall,user)) {
                            println(document.data)
-                           //println(document.data["city"])
-                           val projectNameView = findViewById<TextView>(R.id.ProjectName_mainView)
-                           val teamMemberView = findViewById<TextView>(R.id.TeamMembers_mainView)
-                           val stateView = findViewById<TextView>(R.id.status_mainView)
-                           val taskView = findViewById<TextView>(R.id.TaskName_mainView)
-                           var currentProj = document.data["projectName"]
-                           var currentTask = document.data["task"]
-                           var currentDDL = document.data["deadline"]
-                           var currentState = document.data["state"]
-                           var currentWorker = document.data["worker"]
-                           println(currentProj)
-                           //b.putString("ProjectName", currentProj.toString())
-                           //b.putString("TeamMembers", user)
-                           //b.putString("Status", currentState.toString())
-                           //b.putString("TaskName", currentTask.toString())
-                           projectNameView.text = currentProj as CharSequence?
-                           teamMemberView.text = currentWorker as CharSequence?
-                           stateView.text = currentState as CharSequence?
-                           taskView.text = currentTask as CharSequence?
+                           val tvProjectName = TextView(this)
+                           tvProjectName.text =  name
+                           val tvDeadline = TextView(this)
+                           tvDeadline.text =  projdeadline
+                           //NEED CODE FOR NUMBER OF WORKERS IN AN ARRAY
+                           val tvMang = TextView(this)
+                           tvMang.text =  user //REPLACE WITH MANAGER
 
+                           val list = LinearLayout(this)
+                           list.orientation = VERTICAL
+                           list.addView(tvProjectName)
+                           list.addView(tvDeadline)
+                           list.addView(tvMang)
+                           var cvCard = createCard(workertester,projectlistall,user,list,bt_id)
+
+
+                           linear.addView(cvCard)
+                            cvCard.id=bt_id+1000
+                            bt_id++
+
+                        }else{
+
+                           println(document.data["workers"])
                        }
                    }
                }
@@ -80,9 +159,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                }
 //////////////////////////////////////////////////////////////////////////////////////
 
-           val frag = mainVeiwFrag()
-           frag.arguments = b
-           showFragment(frag)
+
         mBtMainLogout.setOnClickListener(this)
     }
 
@@ -105,12 +182,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 startActivity(intent2)
                 finish()
             }
-        }
-    }
-    fun showFragment(fragment:  mainVeiwFrag){
-        val fram = supportFragmentManager.beginTransaction()
-        fram.replace(R.id.fragmentContainerView,fragment)
-        fram.commit()
+            else ->{
+                var i:Int = view.id
+                println(i)
+                val tv: TextView = findViewById(100+i)
+                val cv: CardView = findViewById(1000+i)
+                println(tv.text)
+            }
+
+       }
     }
 
 }
+
